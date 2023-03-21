899aae62c63cc342ae62b34c02e80ddd1aad08f1
awesunset <125528977+awesunset@users.noreply.github.com>
2023-03-21 17:27:39 +0900

    크리스피크림도넛
    팝업 클릭이벤트 및 메뉴 팝업/썸네일 데이터 입력

---
+++ krispykreamSub3.js
@@ -0,0 +1,52 @@
+$(".frm").submit(function() {
+    // id칸의 공란여부 체크
+    if (!$(".num").val()) {
+        // 빈칸이면
+        $(".mg1").css("display", "block");
+        $(".mg1").focus();
+        // 경고문 출력후 입력포커스 이동
+        return false;}
+
+    // pw칸의 공란여부 체크
+    if (!$(".stday").val()) {
+        // 빈칸이면
+        $(".mg2").css("display", "flex");
+        $(".mg2").focus();
+        // 경고문 출력후 입력포커스 이동
+        return false;
+    }
+
+    if (!$(".time").val()) {
+        // 빈칸이면
+        $(".mg3").css("display", "block");
+        $(".mg3").focus();
+        // 경고문 출력후 입력포커스 이동
+        return false;
+    }
+
+});
+
+// 코멘트 글자수제한
+
+$(document).ready(function() {
+    $('#comment').on('keyup', function() {
+        $('#comment_cnt').html("("+$(this).val().length+" / 300)");
+ 
+        if($(this).val().length > 300) {
+            $(this).val($(this).val().substring(0, 300));
+            $('#comment_cnt').html("(300 / 300)");
+        }
+    });
+});
+
+// 코멘트 글자수 제한 알림
+
+$('#comment').keyup(function (e) {
+	let content = $(this).val();
+    if (content.length > 300) {
+    	// 200자 부터는 타이핑 되지 않도록
+        $(this).val($(this).val().substring(0, 300));
+        // 200자 넘으면 알림창 뜨도록
+        alert('글자수는 300자까지 입력 가능합니다.');
+    };
+});
---
+++ krispykreamSubotherdrink.js
@@ -0,0 +1,111 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+    breakpoints: { //반응형 width
+
+      640: { //640 이상일 경우
+      
+      slidesPerView: 2,},
+      
+      768: { slidesPerView: 3, }, 1024: { slidesPerView: 4, }, }
+  });
+
+
+
+  $.ajax({
+    type: "GET",
+    url:"/ex/4.js/otherdrink.json",
+    dataType: "json",
+    success: function(data) {
+        var elem = "";
+        $.each(data, function() {
+            // this는 각 obj를 의미한다.
+            elem += `<div class="box">`;
+            elem += `<img src='${this.imgfile}' alt='${this.name}'>`;
+                elem += `<span>${this.name}</span>`;
+                elem += `<p>${this.price}</p>`;
+            elem += `</div>`;
+        });
+        $(".sDonut").append(elem);
+    },
+    error: function(xhr) {
+        console.log(xhr.status + "/" + xhr.errorText);
+    }
+  });
+
+
+  $(document).ready(function () {
+
+    //팝업오픈
+    $(".box").on("click", function (event) {  //팝업오픈 버튼 누르면
+  
+  
+  
+  
+  
+    });
+    $(".sDonut .box").each(function (index, value) {
+      
+      $(this).click(function(){
+  
+        $(".popup-wrap").show();
+        $("body").append('<div class="backon"></div>'); //뒷배경 생성
+        $.ajax({
+          type: "GET",
+          url: "/ex/4.js/popup-otherdrink.json",
+          dataType: "json",
+          success: function (data) {         
+            console.log(data[index]);
+            $(".popup-wrap .pimg").attr('src',`${data[index].imgfile}`),
+            $(".cardsec01 h2").html(`${data[index].name}`),
+            $(".cardsec01 p").html(`${data[index].infor}`),
+            $(".cardsec01 h4").html(`${data[index].price}`),
+            $(".tbl-row-info .p01").html(`${data[index].p01}`),
+            $(".tbl-row-info .p02").html(`${data[index].p02}`),
+            $(".tbl-row-info .p03").html(`${data[index].p03}`),
+            $(".tbl-row-info .p04").html(`${data[index].p04}`),
+            $(".tbl-row-info .p05").html(`${data[index].p05}`),
+            $(".tbl-row-info .p06").html(`${data[index].p06}`),
+            $(".cardsec03 .p07").html(`${data[index].p07}`),
+            $(".tbl-row-info .p08").html(`${data[index].p08}`)
+  
+            //나머지채우기
+    
+    
+          },
+          error: function (xhr) {
+            console.log(xhr.status + "/" + xhr.errorText);
+          }
+        });
+      })
+  
+    });
+  
+    //배경클릭시 닫기
+    $("body").on("click", function (event) {
+      if (event.target.className == '.popup-wrap' || event.target.className == 'backon') {
+        $(".popup-wrap").hide(); //close버튼 이거나 뒷배경 클릭시 팝업 삭제
+        $(".backon").hide();
+      }
+    });
+  
+    //상세정보 슬라이드
+    $(".popbt").on("click", function (event) {  //팝업오픈 버튼 누르면
+      $(".cardsec02").slideToggle(300);
+  
+      $("body").on("click", function (event) {
+        if (event.target.className == '.popbt') {
+          $(".cardsec02").click(); //close버튼 이거나 뒷배경 클릭시 팝업 삭제
+        }
+      });
+    });
+  });
